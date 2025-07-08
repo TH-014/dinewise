eefc453a3597b7c70d555e3ba19955ba4e94edee
Turad-011 <2005011@ugrad.cse.buet.ac.bd>
2025-07-08 08:21:55 +0600

    bugfix9

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,57 +1,56 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: '::',
+    port: 8082,
+  },
+  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+}));
+
+
+
+
+
+
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react-swc";
 // import path from "path";
 // import { componentTagger } from "lovable-tagger";
 
-// // https://vitejs.dev/config/
+// import { configDefaults } from 'vitest/config';
+
+
 // export default defineConfig(({ mode }) => ({
 //   server: {
-//     host: '0.0.0.0',
+//     host: "::",
 //     port: 8082,
 //   },
 //   plugins: [
 //     react(),
-//     mode === 'development' &&
-//     componentTagger(),
+//     mode === 'development' && componentTagger(),
 //   ].filter(Boolean),
 //   resolve: {
 //     alias: {
 //       "@": path.resolve(__dirname, "./src"),
 //     },
 //   },
+//   test: {
+//     globals: true,
+//     environment: "jsdom",
+//     setupFiles: "./src/setUpTests.ts", // if you use setup
+//     exclude: [...configDefaults.exclude, '**/e2e/**'],
+//   },
 // }));
-
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-import { configDefaults } from 'vitest/config';
-
-
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8082,
-  },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setUpTests.ts", // if you use setup
-    exclude: [...configDefaults.exclude, '**/e2e/**'],
-  },
-}));
 
 
 // import { defineConfig } from 'vite';
