2e53a6df2203d214ffcfa6421b8d804d549c5734
Turad-011 <2005011@ugrad.cse.buet.ac.bd>
2025-07-08 06:48:22 +0600

    BugFix2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig(({ mode }) => ({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.ts", // if you use setup
+    setupFiles: "./src/setUpTests.ts", // if you use setup
     exclude: [...configDefaults.exclude, '**/e2e/**'],
   },
 }));
