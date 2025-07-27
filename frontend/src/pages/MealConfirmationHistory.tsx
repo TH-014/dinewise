e270e705f7cd74f527955758bebea7540f4a7fa5
Tanvir Hossain <2005014@ugrad.cse.buet.ac.bd>
2025-07-27 21:58:07 +0600

    mealconfirmation bugfix_1

--- frontend/src/pages/MealConfirmationHistory.tsx
+++ frontend/src/pages/MealConfirmationHistory.tsx
@@ -63,7 +63,7 @@ const MealConfirmationHistory = () => {
       }
 
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/manager/showConfirmations}`,
+        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/manager/showConfirmations`,
         {
           method: 'POST',
           credentials: 'include',
